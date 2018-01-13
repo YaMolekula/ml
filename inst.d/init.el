8fec368a4a61a4ff24f900134069d09156778633
ya1krabe@gmail.com <ya1krabe@gmail.com>
2018-01-13 07:46:43 +0000

    improved

---
+++ inst.d/init.el
@@ -0,0 +1,7 @@
+; ipython repl
+(require 'python)
+(setq python-shell-interpreter "ipython")
+;(setq python-shell-interpreter-args "--pylab")
+; C-c C-p - open
+; C-c C-r - send region
+; C-c C-c - run all
