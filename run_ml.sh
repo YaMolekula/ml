f0ae9d93f7ba71a62d819416d023dd184bd1b999
ya1krabe@gmail.com <ya1krabe@gmail.com>
2018-01-29 13:12:57 +0000

    destination path jupyter config file in run_ml.sh

--- run_ml.sh
+++ run_ml.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -p 8888:8888 -v /work:/data -v /work/cfg/jupyter_notebook_config.json:/home/dev/.jupyter/work/cfg/jupyter_notebook_config.json
+sudo docker run -it -p 8888:8888 -v /work:/data -v /work/cfg/jupyter_notebook_config.json:/home/dev/.jupyter/jupyter_notebook_config.json ml
